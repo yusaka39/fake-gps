448ebfe6579f0f11882f04ac85e89a2b9eff2e49
Yuta Sakata <yusaka39@gmail.com>
2016-12-30 20:09:55 +0900

    Modify layout

--- app/src/main/kotlin/io/github/yusaka39/fakegps/MainActivity.kt
+++ app/src/main/kotlin/io/github/yusaka39/fakegps/MainActivity.kt
@@ -2,11 +2,14 @@ package io.github.yusaka39.fakegps
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mapFragment: SupportMapFragment =
+                this.supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
     }
 }
