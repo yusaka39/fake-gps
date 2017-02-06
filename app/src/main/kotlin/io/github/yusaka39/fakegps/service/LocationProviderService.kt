3ff276615a1134e6501f49882aac4d8998f9de05
Yuta Sakata <yusaka39@gmail.com>
2017-02-06 15:12:15 +0900

    Avail mocking for fused location provider

--- app/src/main/kotlin/io/github/yusaka39/fakegps/service/LocationProviderService.kt
+++ app/src/main/kotlin/io/github/yusaka39/fakegps/service/LocationProviderService.kt
@@ -6,11 +6,16 @@ import android.content.Intent
 import android.location.Location
 import android.location.LocationManager
 import android.os.Binder
+import android.os.Bundle
 import android.os.IBinder
 import android.os.SystemClock
 import android.util.Log
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderApi
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
 import java.util.Timer
+import java.util.concurrent.CountDownLatch
 import kotlin.concurrent.timer
 
 /**
@@ -31,6 +36,7 @@ class LocationProviderService :
     }
 
     private var timer: Timer? = null
+    private var apiClient: GoogleApiClient? = null
 
     override fun onBind(intent: Intent?): IBinder = LocationBinder()
 
@@ -42,9 +48,14 @@ class LocationProviderService :
         }
 
         this.timer?.cancel()
+        this.disconnectClientIfNeeded()
+
+        this.apiClient = buildApiClient(latlng)
+        this.apiClient?.connect()
 
         val lm = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         this.prepareTestProviders(lm)
+
         this.timer = timer(period = 5000L) {
             Log.d(LocationProviderService::class.java.name, "Update location")
             MOCKING_TARGET.filter { lm.isProviderEnabled(it) }
@@ -55,8 +66,41 @@ class LocationProviderService :
 
     override fun onDestroy() {
         this.timer?.cancel()
+        this.disconnectClientIfNeeded()
         super.onDestroy()
     }
+
+    private fun disconnectClientIfNeeded() {
+        val client = this.apiClient ?: return
+        if (client.isConnected || client.isConnecting) {
+            client.disconnect()
+        }
+    }
+
+    private fun buildApiClient(latlng: LatLng) =
+            GoogleApiClient.Builder(this).addApi(LocationServices.API)
+                    .addConnectionCallbacks(object : GoogleApiClient.ConnectionCallbacks {
+                        private var timer: Timer? = null
+
+                        override fun onConnected(p0: Bundle?) {
+                            val api = LocationServices.FusedLocationApi
+                            api.setMockMode(this@LocationProviderService.apiClient, true)
+
+                            this.timer = timer(period = 5000L) {
+                                MOCKING_TARGET.forEach {
+                                    api.setMockLocation(this@LocationProviderService.apiClient,
+                                                        this@LocationProviderService
+                                                                .createLocationFor(it, latlng))
+
+                                }
+                            }
+                        }
+
+                        override fun onConnectionSuspended(p0: Int) {
+                            this.timer?.cancel()
+                        }
+                    })
+                    .build()
 
     private fun prepareTestProviders(lm: LocationManager) {
         MOCKING_TARGET.forEach {
@@ -70,13 +114,15 @@ class LocationProviderService :
         }
     }
 
+    private fun createLocationFor(provider: String, latLng: LatLng) = Location(provider).apply {
+        this.latitude = latLng.latitude
+        this.longitude = latLng.longitude
+        this.accuracy = 0f
+        this.time = System.currentTimeMillis()
+        this.elapsedRealtimeNanos = SystemClock.elapsedRealtimeNanos()
+    }
+
     private fun updateLocation(lm: LocationManager, providerName: String, latLng : LatLng) {
-        val location = Location(providerName)
-        location.latitude = latLng.latitude
-        location.longitude = latLng.longitude
-        location.accuracy = 0f
-        location.time = System.currentTimeMillis()
-        location.elapsedRealtimeNanos = SystemClock.elapsedRealtimeNanos()
-        lm.setTestProviderLocation(providerName, location)
+        lm.setTestProviderLocation(providerName, this.createLocationFor(providerName, latLng))
     }
 }
